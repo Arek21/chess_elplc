6142195358699b258f78cb8a17e60660400300eb
Arkadiusz Suda <31943@student.pwsztar.edu.pl>
2021-06-28 13:23:43 +0200

    added interface and class

--- Chess/Form1.cs
+++ Chess/Form1.cs
@@ -16,7 +16,151 @@ namespace Chess
         public Form1()
         {
             InitializeComponent();
+        }
+    }
 
+    enum columns
+    {
+        cA,
+        cB,
+        cC,
+        cD,
+        cE,
+        cF,
+        cG,
+        cH
+    }
+    enum rows
+    {
+        r1,
+        r2,
+        r3,
+        r4,
+        r5,
+        r6,
+        r7,
+        r8
+    }
+
+    public class Position
+    {
+        public int row;
+        public int col;
+
+        public Position(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+    }
+    public class Piece
+    {
+        public Position position;
+        public bool colour;
+
+    }
+    interface IPiece
+    {
+        public void Move();
+        public void ActualPosition();
+    }
+    public class King : Piece, IPiece
+    {
+        public King()
+        {
+            this.position = new Position(2, 2);
+            this.colour = false;
+        }
+        public void IsCheck()
+        {
+
+        }
+        public void IsMat()
+        {
+
+        }
+        public void IsPat()
+        {
+
+        }
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ActualPosition()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Queen : IPiece
+    {
+        private bool colour;
+        private bool isCaptured;
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ActualPosition()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Bishop : IPiece
+    {
+        private bool colour;
+        private bool isCaptured;
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ActualPosition()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Knight : IPiece
+    {
+        private bool colour;
+        private bool isCaptured;
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ActualPosition()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Rock : IPiece
+    {
+        private bool colour;
+        private bool isCaptured;
+        public void ActualPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Pawn : IPiece
+    {
+        private bool colour;
+        private bool isCaptured;
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ActualPosition()
+        {
+            throw new NotImplementedException();
         }
     }
 }
